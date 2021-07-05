25fa8460552997d6856adcb4489b4739518934ca
xuekesong <15235592139@126.com>
2021-07-05 16:03:16 +0800

    新建地址页面布局

--- pages/me/setting/address/make.vue
+++ pages/me/setting/address/make.vue
@@ -50,17 +50,36 @@
       <van-cell title="Label" title-class="light-grey" ></van-cell>
       <van-field v-model="tag" label="文本" label-width="0.72rem">
         <template #input>
-          <span class="fs-14 plr-12 bg-orange round-8 lh-18 tag active mr-12">Default</span>
-          <span class="fs-14 plr-12 bg-orange round-8 lh-18 tag mr-12">Default</span>
-          <!-- 添加标签按钮 -->
-          <BmImage
-            :url="require('@/assets/images/icon/add-icon.svg')"
-            :width="'1.24rem'" 
-            :height="'0.64rem'"
-            :isLazy="false"
-            :isShow="false"
-            @click="addTagFun"
-          ></BmImage>
+          <div class="flex flex-wrap">
+            <div class="fs-14 plr-12 bg-orange round-8 tag active mr-12 mb-20">Default</div>
+            <div class="fs-14 plr-12 bg-orange round-8 tag mr-12 mb-20">Default</div>
+            <!-- 添加标签按钮 -->
+            <div @click="isEmit = 1" v-if="isEmit === 0" class="mb-20">
+              <BmImage
+                :url="require('@/assets/images/icon/add-icon.svg')"
+                :width="'1.24rem'" 
+                :height="'0.64rem'"
+                :isLazy="false"
+                :isShow="false"
+              ></BmImage>
+            </div>
+            <!-- 添加标签时的展示样式 -->
+            <van-field
+              v-model="emitTag"
+              placeholder="Input label name"
+              :class="{'p-0 border round-8 hidden custom-field mb-20 vcenter': true}"
+              v-else-if="isEmit === 1"
+            >
+              <template #button>
+                <van-button size="small" type="primary" :class="{'bg-grey': emitTag.length === 0, 'bg-green': emitTag.length > 0}" @click="isEmit = 2">Add</van-button>
+              </template>
+            </van-field>
+            <!-- 添加之后的标签展示样式 -->
+            <div class="flex round-8 mb-20 hidden border-green" v-else-if="isEmit === 2">
+              <span class="plr-12 fs-14 green ptb-3">{{ emitTag }}</span>
+              <span class="plr-12 fs-14 bg-green white ptb-3" @click="isEmit = 1">editor</span>
+            </div>
+          </div>
         </template>
       </van-field>
       <!-- 设置默认地址 -->
@@ -95,13 +114,12 @@ export default {
       address: '',
       detail: '',
       tag: 'Home',
-      isDefault: true
+      isDefault: true,
+      emitTag: '',
+      isEmit: 0 // 0 需要添加 1 添加中 2添加完成
     }
   },
   methods: {
-    addTagFun() { // 添加标签
-
-    },
     save() { // 保存地址
       // 地址保存成功跳转到地址列表页面
       this.$router.replace({
@@ -127,15 +145,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.lh-18{
-  line-height: 18px;
-}
 .tag{
   border: 1px solid #BFBFBF;
   background-color: #fff;
   color: #666;
   padding-top: 6px;
   padding-bottom: 6px;
+  line-height: 18px;
+  height: 32px;
   &.active{
     background-color: rgba(66, 183, 174, 0.05);
     border: 1px solid #42B7AE;
@@ -144,5 +161,27 @@ export default {
 }
 .save-btn{
   margin-top: 116px;
+}
+.border-green{
+  border: 1px solid #42B7AE;
+}
+.ptb-3{
+  padding-top: 3px;
+  padding-bottom: 3px;
+}
+</style>
+
+<style lang="less">
+.custom-field{
+  width: 256px;
+  height: 32px;
+  .van-button{
+    width: 60px;
+    height: 32px;
+    border: none;
+  }
+  .van-field__control{
+    padding-left: 10px;
+  }
 }
 </style>
