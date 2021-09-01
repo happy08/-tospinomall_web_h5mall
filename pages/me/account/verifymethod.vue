bb6268096b30f4983d6ffaab886bc7abcb2de2c1
xuekesong <15235592139@126.com>
2021-09-01 18:48:55 +0800

    修改文案

--- pages/me/account/verifymethod.vue
+++ pages/me/account/verifymethod.vue
@@ -4,7 +4,7 @@
     <BmHeaderNav :left="{ isShow: true }" :title="$t('verification_method')" />
 
     <!-- 登录密码验证 -->
-    <van-cell class="ptb-20 plr-20" :title="$t('use_current_password_auth')" is-link title-class="black" :to="{ name: $route.query.type == 'pay' ? 'me-pay-payPwd' : 'me-account-verify' }" v-if="$route.query.type != 'payForget'" />
+    <van-cell class="ptb-20 plr-20" :title="$t('verify_by_password')" is-link title-class="black" :to="{ name: $route.query.type == 'pay' ? 'me-pay-payPwd' : 'me-account-verify' }" v-if="$route.query.type != 'payForget'" />
     <!-- 短信验证 -->
     <van-cell v-if="$store.state.user.userInfo.phone" class="ptb-20 plr-20" :title="$t('verify_by_sms')" is-link title-class="black" :to="{ name: $route.query.type == 'pay' || $route.query.type == 'payForget' ? 'me-pay-code' : 'me-account-verify-code', query: { from: $route.query.from } }" />
     <!-- 电子邮件验证 -->
