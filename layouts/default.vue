8087d1f5dc5a96c462fc45334480077ca19122f1
xuekesong <15235592139@126.com>
2021-09-01 18:42:43 +0800

    修改个人信息出生年份限制

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,24 @@
 <template>
   <!-- <div> -->
     <!-- <Nuxt keep-alive :keep-alive-props="{ max: 3 }" /> -->
-    <Nuxt keep-alive />
+    <Nuxt keep-alive v-show="isLoad" />
+    <!-- <div v-else class="flex vcenter hcenter v-percent-100">
+      <img class="w-128" src="@/assets/images/home-loading.png" alt="loading picture">
+    </div> -->
+    <!-- <Nuxt /> -->
   <!-- </div> -->
 </template>
 
 <script>
 export default {
-  name: 'LayOuts'
+  name: 'LayOuts',
+  data() {
+    return {
+      isLoad: false
+    }
+  },
+  mounted() {
+    this.isLoad = true;
+  }
 }
 </script>
