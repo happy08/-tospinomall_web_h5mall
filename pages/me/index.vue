dd907fe78ea313c780878cd58064e104b042ba4e
xuekesong <15235592139@126.com>
2021-09-01 17:15:29 +0800

    修改文案

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -44,7 +44,7 @@
         <nuxt-link :to="$store.state.user.authToken ? { name: 'me-likes' } : { name: 'login' }" v-slot="{ navigate }" class="tc">
           <dl @click="navigate" role="link">
             <dt class="fs-24 black fw">{{ $store.state.user.userInfo ? $store.state.user.userInfo.attentionProductNum : 0 }}</dt>
-            <dd class="fs-12 grey mt-4">{{ $t('collect') }}</dd>
+            <dd class="fs-12 grey mt-4">{{ $t('collection') }}</dd>
           </dl>
         </nuxt-link>
         <nuxt-link :to="$store.state.user.authToken ? { name: 'me-wallet' }: { name: 'login' }" v-slot="{ navigate }" class="tc">
