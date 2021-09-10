04ad6db371d9b6e22f49df0ababca3e0ebebfcc1
xuekesong <15235592139@126.com>
2021-09-10 14:37:58 +0800

    修改字段

--- pages/me/account/bind/rebind.vue
+++ pages/me/account/bind/rebind.vue
@@ -142,6 +142,7 @@ export default {
       if (this.$route.query.changeWay === 'email') { // 获取邮箱验证码
         let reg = /^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\.)+[a-zA-Z]{2,}$/;
         if (!reg.test(this.account)) {
+          this.isCodeFlag = false;
           this.$toast(this.$t('email_format_error'));
           return false;
         }
