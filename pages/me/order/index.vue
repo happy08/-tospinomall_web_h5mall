b8211a5034d2b4eb67a445e4dbb2df4a65b4c33a
xuekesong <15235592139@126.com>
2021-09-25 14:15:32 +0800

    订单列表拒收状态订单可删除

--- pages/me/order/index.vue
+++ pages/me/order/index.vue
@@ -69,7 +69,7 @@
                     <!-- 订单店铺 -->
                     <OrderStoreSingle :name="item.storeName" :status="statusFormat(item.status, item)" @goStoreDetail="goStoreDetail(item)">
                       <!-- 如果是取消状态，则该订单可删除，添加操作展示  -->
-                      <div slot="other-deal" class="flex vcenter" v-if="item.status == 5 || item.status == 6 || item.status == 4">
+                      <div slot="other-deal" class="flex vcenter" v-if="item.status == 5 || item.status == 6 || item.status == 4 || item.status == 7">
                         <span class="block line-style"></span>
                         <BmImage 
                           :url="require('@/assets/images/icon/delete-icon.svg')"
@@ -592,7 +592,7 @@ export default {
       })
     },
     statusFormat(val, item) {
-      return val == 0 ? this.$t('unpaid') : val == 1 ? this.$t('to_be_delivered') : val == 2 ? this.$t('unreceived') : val == 3 || val == 4 ? this.$t('completed') : val == 5 ? this.$t('cancelled') : val == 6 ? this.$t('trading_close') : val == 7 ? this.$t('un_rejected') : this.$t('other');
+      return val == 0 ? this.$t('unpaid') : val == 1 ? this.$t('to_be_delivered') : val == 2 ? this.$t('unreceived') : val == 3 || val == 4 ? this.$t('completed') : val == 5 ? this.$t('cancelled') : val == 6 ? this.$t('trading_close') : val == 7 ? this.$t('trading_close') : this.$t('other');
     },
     goStoreDetail(storeItem) { // 跳转到店铺首页
       this.$router.push({
