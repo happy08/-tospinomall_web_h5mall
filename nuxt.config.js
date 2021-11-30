a245ae969e8609b75c5fa5685a97bd5da96712fc
xuekesong <15235592139@126.com>
2021-11-30 19:47:02 +0800

    修改配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,17 +103,6 @@ export default {
         '^/apiLogin': '/'
       }
     },
-    // '/api/order/pay/order': {
-    //   // target: 'http://192.168.2.34:9999', //120.78.145.142
-    //   // target: 'http://192.168.2.70:9999',
-    //   // target: 'https://tospinomallapi.fyynet.com',
-    //   // target: 'http://192.168.2.35:9999',
-    //   //target: 'http://120.78.145.142:9999',
-    //   target: 'http://rnzsgf7l.dongtaiyuming.net',
-    //   pathRewrite: {
-    //     '^/api/order/pay/order': '/order/pay/order'
-    //   }
-    // },
     '/api': {
       // target: 'http://192.168.2.34:9999', //120.78.145.142
       // target: 'http://192.168.2.70:9999',
