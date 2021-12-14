1237e037784befc48ba000a4fc516ca3fcbabdb4
xuekesong <15235592139@126.com>
2021-12-14 14:29:55 +0800

    所有商品列表发货地logo不展示

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -154,7 +154,7 @@
             <nuxt-link :to="'/product/' + searchItem.productId + '.html'" v-for="(searchItem, searchIndex) in recommendList" :key="'search-list-' + searchIndex" class="mb-12 custom-grid-item" v-masonry-tile>
               <ProductTopBtmSingle
                 :img="{ url: searchItem.mainPictureUrl, width: '3.4rem', height: '3.4rem', loadImage: require('@/assets/images/product-bgd-170.png') }" 
-                :detail="{ desc: searchItem.productTitle, price: searchItem.minPrice, rate: parseFloat(searchItem.starLevel), volumn: searchItem.saleCount, ellipsis: 2, country: searchItem.supplyCountryName, country_url: searchItem.supplyCountryIcon, stock: searchItem.stock }"
+                :detail="{ desc: searchItem.productTitle, price: searchItem.minPrice, rate: parseFloat(searchItem.starLevel), volumn: searchItem.saleCount, ellipsis: 2, /*country: searchItem.supplyCountryName, country_url: searchItem.supplyCountryIcon, */stock: searchItem.stock }"
               />
             </nuxt-link>
             
